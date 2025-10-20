00aca5027a5af09e34867944e068a9ac6a190a97
Tayyab Khan <97843826+easy2-code@users.noreply.github.com>
2025-10-20 12:12:11 +0500

    Complete Sign up and Sign in page...

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,5 +1,44 @@
-import React from "react";
+import CommonForm from "@/components/common/CommonForm";
+import { loginFormControls } from "@/config";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 export default function Login() {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState(initialState);
+
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log("Form Data Submitted:", formData);
+  }
+
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Sign in to your account
+        </h1>
+        <p className="mt-2">
+          Don't have an account?
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/register"
+          >
+            Sign up
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={loginFormControls}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+        buttonText="Sign In"
+      />
+    </div>
+  );
 }
