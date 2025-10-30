5085f90975d3e519be6c3576c390c52c8b526494
Tayyab Khan <97843826+easy2-code@users.noreply.github.com>
2025-10-30 11:10:05 +0500

    Complete only ui  of shopping listings and filter page...

---
+++ frontend/src/components/shopping-view/ProductFilter.jsx
@@ -0,0 +1,36 @@
+import { filterOptions } from "@/config";
+import React, { Fragment } from "react";
+import { Label } from "../ui/label";
+import { Checkbox } from "../ui/checkbox";
+import { Separator } from "../ui/separator";
+
+export default function ProductFilter() {
+  return (
+    <div className="bg-background rounded-lg shadow-sm">
+      <div className="p-4 border-b">
+        <h2 className="text-lg font-extrabold mb-1">Filters</h2>
+      </div>
+      <div className="p-4 space-y-4">
+        {Object.keys(filterOptions).map((keyItem) => (
+          <Fragment key={keyItem}>
+            <div>
+              <h3 className="text-base font-bold">{keyItem}</h3>
+              <div className="grid gap-2 mt-2">
+                {filterOptions[keyItem].map((option) => (
+                  <Label
+                    key={option.id || option.label} // ✅ unique key for each label
+                    className="flex font-medium items-center gap-2"
+                  >
+                    <Checkbox />
+                    {option.label}
+                  </Label>
+                ))}
+              </div>
+            </div>
+            <Separator />
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
+}
