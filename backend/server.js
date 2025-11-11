5f8ced37afda42becd3279cf7af2ac45a6682c57
Tayyab Khan <97843826+easy2-code@users.noreply.github.com>
2025-11-11 15:21:14 +0500

    Server Updated..

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,7 @@ const PORT = process.env.PORT || 3000;
 // CORS Setup
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://loopmart-frontend.vercel.app", // <-- update with your actual Vercel domain
-    ],
+    origin: ["http://localhost:5173", "https://loopmart-lac.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: [
